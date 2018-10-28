1a81edb9df368fd344d11d39b194704fe8424010
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 20:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-var katzDeliLine = []
+var deliLine = []
 
 function takeANumber( line, name ) {
     line.push( name )
-    return `Welcome, ${ name }. You are number ${ line.length } in line.`
+    return `Welcome, ${ name }.  You are number ${ line.length } in line.`
 }
 
 function nowServing( line ) {
-  if( line.length > 1 )
-    return `Currently serving ${ line.shift() }.`
-  else
-    return 'There is nobody waiting to be served!'
+
 }
 
 function currentLine( line ) {
-  var newLine = line.reduce( ( list, person, i, line ) => list + `${ i + 1 }. ${ person }, `, 'The line is currently: ')
-  return newLine.slice( 0, newLine.length - 1 )
+
 }
