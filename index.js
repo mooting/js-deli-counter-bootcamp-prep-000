ef196ad1901f9c4b7cbe044e821e0d07b7704301
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 20:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var deliLine = []
 
 function takeANumber( line, name ) {
     line.push( name )
-    return `Welcome, ${ name }.  You are number ${ line.length } in line.`
+    return `Welcome, ${ name }. You are number ${ line.length } in line.`
 }
 
 function nowServing( line ) {
